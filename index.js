457999520f532aaac680aaecfb9df4081898d453
Shoaib <shoaib.ultracodes@gmail.com>
2022-09-26 22:14:46 +0500

    Auth Routes

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 
 const port = process.env.PORT || 5001;
 
+const authRoute = require('./src/Routes/auth.route');
+
 // Routes
+app.use("/auth", authRoute);
 
 app.listen(port, ()=> {
     console.log(`Server running on port: ${port}`);

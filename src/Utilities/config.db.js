29c8d3f60d876ba1bfe4e37282344a66d093313c
ammaralizahid <amilzak42@gmail.com>
2023-02-26 13:16:54 +0500

    db config

--- src/Utilities/config.db.js
+++ src/Utilities/config.db.js
@@ -1,22 +1,22 @@
 const mysql = require('mysql');
 
-const conn = mysql.createPool({
-  host: "localhost",
-    user:"root",
-    password:"", 
-    database: "pakloaders",
-    port: 3306 
-});
+// const conn = mysql.createPool({
+//   host: "localhost",
+//     user:"root",
+//     password:"", 
+//     database: "pakloaders",
+//     port: 3306 
+// });
 
 // umer rasheed credentials
 
-// const conn = mysql.createPool({
-//   host: "sql6.freemysqlhosting.net",
-//   user:"sql6590784",
-//   password:"UepK2CveRe", 
-//   database: "sql6590784",
-//   port: 3306 
-// });
+const conn = mysql.createPool({
+  host: "sql6.freemysqlhosting.net",
+  user:"sql6590784",
+  password:"UepK2CveRe", 
+  database: "sql6590784",
+  port: 3306 
+});
 
 // const conn = mysql.createPool({
 //   host: "sql12.freemysqlhosting.net",

d43cad4d29856595d5af5c27ada3499ad0c5b804
ammaralizahid <amilzak42@gmail.com>
2023-03-14 00:21:48 +0500

    push notifications

--- src/Controllers/bids.controller.js
+++ src/Controllers/bids.controller.js
@@ -1,5 +1,5 @@
 const Bids = require('./../Models/bids.model');
-
+const {sendPushNotification} = require('../Utilities/pushNotifications')
 const getBids = async(req, res, next)=> {
     try {
         Bids.getBids((err, response)=> {
@@ -32,11 +32,39 @@ const getBidDetails = async(req, res, next)=> {
 const createBid = async(req, res, next)=> {
     try {
         const result = new Bids(req.body);
+        const body='Sent you a bid proposal'
         Bids.createBid(result, (err, response)=> {
             if(err){
                 next(err);
             } else {
-                res.status(200).send({message: "Bid Placed successfully!"});
+                Bids.userDeviceToken(result.post_id,(err,tokenResponse)=> {
+            if(err){
+            next(err)
+            }else{
+    Bids.DriverName(result.driver_id,(err,driverName)=>{
+        if(err){
+            next(err)
+        }else{
+
+            const tokens = tokenResponse.map((i) => i.device_token);
+            for (let i = 0; i < tokens.length; i++) {
+              const token = tokens[i];
+              sendPushNotification(token, result.driver_id, driverName[0].full_name, body, async (err, notificationResponse) => {
+                if (err) {
+                  next(err);
+                } else {
+                  console.log(`Notification sent to token ${token}:`, notificationResponse);
+                  if (i === tokens.length - 1) {
+                    // This is the last iteration of the loop, so send the response to the client
+                    res.status(200).send({message: "Bid Placed successfully!"});
+                  }
+                }
+              });
+            }
+        }
+    })
+}
+                })
             }
         })
     } catch (error) {
---
+++ src/Utilities/privateFile.js
@@ -0,0 +1,13 @@
+const privateFile={
+  "type": "service_account",
+  "project_id": "pakloadercustomerapp",
+  "private_key_id": "fc3750857f598450724047924186fc3fcbb29d24",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC+tkDxelUo+bbS\n3QldYYYAdUY3jpmWEGjQWoonSa9Objtsuh6BIDiILMoQn+fZ0aLSdImvPlb7DSIl\n0qjQcqTV7E2v73kCqzof0B6Oj9oWV9AwXimMmoxI/Jmpow5jnHPY24v1wmDrLk08\nS6Ss6a90DRwutElo1KTQGlyaaEnzP7DbFSLg+OjKLFLXogW0DQIoWYy/a4UxZKo9\nvvBc5IlfGNyoai49ykuiGDYUfowfTK8e1Tfk9hyE4N6YWH497djoTwkfX4Zv+aPF\nwh4TO8Nac7TH9hNUDrFvIOJEex+Q7QKzegVKPfgn5iKrH3SeMSoXejiBi3zZn3CY\n3phxK4WrAgMBAAECggEAGqO1taV7cNJU5eaHUNp3D+hRyx/pt0sKgRQsmSlTShbe\nySTEbxATpX3wKdtUVRpR7qjltKt2FfJzMypWd2uUN+eewHUoh6VoZJo7MnnIondY\ntvDoR+Ist2NFiasjruljS0b7qXQh+qgIlOQSGC86WXEdXy7C1Fhjv0f3aIAp2Ebu\n2Eb/m3I7O1RD0m6MnUscRNiJhpaVSEkyCS0L9Djthi7O/N2qztVuUU3zYHYioZhs\n+r1jYSX6YUROe9zPwRVnhlEXTjxX673yssw1O1cAJZgCDfxj1aW4v8I9e9U7sFVb\neY98+uXxImznEk6s3P3Hce9aGugxVpbZ4Ul6xrmYlQKBgQDwU+VyOhK41Qcj6nmw\n00l5S/ueYooh5qOcR9KNZs4duMjge9xl0huN7qyctMMAmuo5ibOBNOG5iqVEtIRT\nR8y5UZnk61B/s6zIocdeDY/o2K9FKtcqFdwbVVnL6x2qPmfH/K6iTchgbwIYAQWR\nACY3tBluctWAllzHa4ELo8C5DQKBgQDLJg5t/IRD2kHwwYz5XnGG/QjlHfizJeI5\nrC3R0Y4vYiA+vQcLrMp9z0PtMx11rzl2H4uONVV8p+oXh3traIb7DAr9GZmnx7MK\nILlpFFwu8VdF2V8fUzYVs+dZ9wWxAq5JU+ZCYgOjCZvkxE+Tc+kR2ytR/DCsG+GY\n6XxdcGIblwKBgHU8V/eLAMsNkHgiz85Eo9j6CfrPkxbKjhGUr8+u0OmK1OM9SNya\nzDpGrgmb3GyVJaF/3QMwqtR6H/kOHmcJ5YKZnfsgF5rUKXuN/9WPxB4QZh/VsDSl\niUQENNPXFBeSbcoPJ5mhXBg+zsSq6u47JmmR/73zYtdhgkz90mu9LCyZAoGBAJo1\nsFxzhJ/RTZ8J/g7KT5mt+RObtc+gYWNwPXP6BFoZveqCKZgGOcxZCvJDjAhq1E7D\n0XdJKzSubnggA2NcfKKT2L+asFZ5tIJqpvSKXRPRZnOCd7/atcKxx1w2tR5Sr6TA\nSJl5t9MKl0mIb/O52GISoobgzr3HiHBARYTUq9TPAoGBAOq0KSxrw4uTcqG2vuaC\nhSNZavuxovr4q1DpqAGnvwq+qZKJMlY64mVoLHdNOiBhu3BWyqubpTVL9ye3NSs4\nRJTU9O9T02P/e0ziVMyqXmqhlrjcrnEQW45O2XK3hzpAiSbe2t2/CzZa0N4cBq4n\ns0oPRkNJEbS+Okz+lG5ER7Ce\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-je7lp@pakloadercustomerapp.iam.gserviceaccount.com",
+  "client_id": "109170136955661695488",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-je7lp%40pakloadercustomerapp.iam.gserviceaccount.com"
+}
+module.exports={privateFile}
